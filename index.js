81c6bac9fd400b0a34830ca7d8d86e5a91c02ef4
Isabella Costabile <costabileisa@gmail.com>
2022-02-16 23:43:27 -0500

    commit tree 5f1c04a5060f29a5779cc87f9961847bb9b7c48e

---
+++ index.js
@@ -0,0 +1,39 @@
+// adds a and b
+function add(a, b) {
+    return a + b;
+}
+
+// subtracts b from a
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiplies a and b
+function multiply(a, b) {
+    return a * b;
+}
+
+// divides b from a
+function divide(a, b) {
+    return a / b;
+}
+
+// adds 1
+function increment(a) {
+    return a + 1;
+}
+
+// subtracts 1
+function decrement(a) {
+    return a - 1;
+}
+
+// turns `n` into an integer
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+// turns `n` into a float
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
